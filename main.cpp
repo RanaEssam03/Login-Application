f3a94cb68769516748daa99fe1d742e1c4177e59
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2022-04-28 21:14:08 +0200

    main menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello world";
+    cout << "\t\tWelcome to the Application " << char(1) << endl;
+    while (true){
+    cout << "Please pick one choice : ";
+    cout << "\n1.Register\n2.Login\n3.Change Password\n4.Exit";
+    int choice;
+
+    cin >> choice;
+//    if (choice == 1)
+//        register();
+//    else if (choice == 2)
+//        login();
+//    else if (choice == 3)
+//        changePassword();
+    if (choice == 4)
+        break;
+    }
+
 }
+
 
